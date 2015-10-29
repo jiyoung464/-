50f70a76ab84033d7cbee1ba485fb067d00f5b58
SeongKyeong <SeongKyeong@MinSeongKyeong>
2015-10-29 23:01:33 +0900

    학생정보등록

--- SoGong_1/src/AddStudent.java
+++ SoGong_1/src/AddStudent.java
@@ -1,29 +1,9 @@
 class AddStudent
 {
-	public static void main(String[] args)
-{System.out.println("www");
+	public void addStudent()
+	{
+		System.out.println("학생정보를 등록합니다.");
+        System.out.println("학번 이름 학과 전화번호 순으로 적어주세요. ");
+        System.out.println("학번	이름	 학과	 전화번호 ");
+	}
 }
-}
-
-/*
-import java.util.Scanner;
-
-public class AddStudent {
-
-		public static void main(String[] args) 
-		{
-			System.out.println("ggg");
-			//String source = "1 5 7";
-			//Scanner sc = new Scanner(source);
-
-			//int num1=sc.nextInt();
-			//int num2=sc.nextInt();
-			//int num3=sc.nextInt();
-			//int sum = num1+num2+num3;
-
-			//System.out.printf("문자열에 저장된 %d, %d, %d의 합은 %d \n", num1, num2, num3, sum);
-		}
-}
-
-
-*/
