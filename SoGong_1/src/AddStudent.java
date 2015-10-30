af1d72c36f6c089db0e6c2ca295149cbbec92e9c
SeongKyeong <SeongKyeong@MinSeongKyeong>
2015-10-30 17:14:26 +0900

    학생정보등록

--- SoGong_1/src/AddStudent.java
+++ SoGong_1/src/AddStudent.java
@@ -4,6 +4,6 @@ class AddStudent
 	{
 		System.out.println("학생정보를 등록합니다.");
         System.out.println("학번 이름 학과 전화번호 순으로 적어주세요. ");
-        System.out.println("학번	이름	 학과	 전화번호 ");
+        System.out.println("학번	이름	 학과	 전화번호");
 	}
 }
