b565419f95405043b2a62ec42d7be57b9ec9149b
SeongKyeong <SeongKyeong@MinSeongKyeong>
2015-10-30 17:49:20 +0900

    main함수 틀만 만들고, AddStudent함수 수정했어요.

--- SoGong_1/src/AddStudent.java
+++ SoGong_1/src/AddStudent.java
@@ -1,14 +1,33 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 class AddStudent
 {
-	public void addStudent()
+	public void addStudent()throws IOException{
 	{
 		Scanner scan = new Scanner(System.in);
         String fpath = "C:" + "studentlist.txt";
         
-		System.out.println("학생정보를 등록합니다.");
-        System.out.println("학번 이름 학과 전화번호 순으로 적어주세요. ");
-        System.out.println("학번	이름	 학과	 전화번호");
-	}
+        try {
+
+            PrintWriter out = new PrintWriter(new FileWriter(fpath, true));
+               String str = scan.nextLine();
+			   out.println(str);
+     
+        out.close();
+     
+        } catch (Exception e) {
+    
+           if(e instanceof IOException) System.out.println("Input & Out Error ");
+           else System.out.println("Etc Error " + e.toString());
+
+
+        } 
+
+    } 
+   } 
 }
+
+
---
+++ SoGong_1/src/Main.java
@@ -0,0 +1,12 @@
+
+	public class Main
+	{
+		public static void main(String[] args)
+		{
+			System.out.println("1)  학생 등록");
+			System.out.println("2)  학생 검색");
+			System.out.println("3)  학생 삭제");
+			System.out.println("4)  학생 기록 수정");
+			System.out.println("5)  프로그램 종료");
+		}
+}
