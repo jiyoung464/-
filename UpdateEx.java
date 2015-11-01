ef6ed643a2262d663954e70d0e00726327ee3dfa
jiyoung464 <jiyoung4646@gmail.com>
2015-11-01 19:17:47 +0900

    rebase test

--- UpdateEx.java
+++ UpdateEx.java
@@ -1,5 +1,16 @@
-class UpdateEx{
+import java.io.*;
+import javax.swing.*;
+
+class Student
+{
+   String studentnum;
+   String name;
+   String department;
+   String phonenum;
+}
+
+public class UpdateEx{
 	public static void main(String[] args){
-		System.out.println("test");
+		System.out.println("rebase test");
 	}
 }
